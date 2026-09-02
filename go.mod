d9dbe3afad9d9c0a3a04c093d9c92bb2da8d4f97
Michael Chisari <michael@chisari.us>
2026-09-01 21:35:23 -0400

    Initial routes and templates

---
+++ go.mod
@@ -0,0 +1,8 @@
+module michaelchisari/NAF
+
+go 1.26.6
+
+require (
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"io/fs"
+	"log"
+	"net/http"
+	"path"
+	"strings"
+
+	"github.com/valyala/fasttemplate"
+)
+
+const SERVER_PORT = ":9003"
+const TPL_OPEN = "<? "
+const TPL_CLOSE = " ?>"
+
+//go:embed templates/pages/*
+var tplPagesFiles embed.FS
+var tplPages = make(map[string]string)
+
+//go:embed templates/components/*
+var tplComponentsFiles embed.FS
+var tplComponents = make(map[string]string)
+
+func init() {
+	// Pages
+	pages, err := fs.Glob(tplPagesFiles, "templates/pages/*.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, pagePath := range pages {
+		pageContent, err := tplPagesFiles.ReadFile(pagePath)
+		if err != nil {
+			log.Fatalf("Error reading embedded files: %s: %v\n", pagePath, err)
+		}
+		p := path.Base(pagePath)
+		i := strings.TrimSuffix(p, ".html")
+		tplPages[i] = string(pageContent)
+	}
+
+	// Components
+	components, err := fs.Glob(tplComponentsFiles, "templates/components/*.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, componentPath := range components {
+		componentContent, err := tplComponentsFiles.ReadFile(componentPath)
+		if err != nil {
+			log.Fatalf("Error reading embedded files: %s: %v\n", componentPath, err)
+		}
+		p := path.Base(componentPath)
+		i := strings.TrimSuffix(p, ".html")
+		fmt.Println("INDEX:", i)
+		tplComponents[i] = string(componentContent)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handleHubAtRoot)
+	http.HandleFunc("/navigation", handleNavigationPage)
+
+	log.Println("Server starting on", SERVER_PORT)
+	if err := http.ListenAndServe(SERVER_PORT, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handleHubAtRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hub")
+}
+
+func handleNavigationPage(w http.ResponseWriter, r *http.Request) {
+	headerNavComponent := tplComponents["header_nav"]
+	navigationPage := tplPages["navigation"]
+
+	t := fasttemplate.New(navigationPage, TPL_OPEN, TPL_CLOSE)
+	s := t.ExecuteString(map[string]any{
+		"header_nav": headerNavComponent,
+	})
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, s)
+}
